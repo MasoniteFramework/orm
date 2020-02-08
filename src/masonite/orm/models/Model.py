40ecfd2b20641866529360236dda0cd66c2e8068
Joseph Mancuso <idmann509@gmail.com>
2020-02-08 00:28:46 -0500

    added model methods

---
+++ src/masonite/orm/models/Model.py
@@ -0,0 +1,16 @@
+class Model:
+    
+    def first(self):
+        pass
+
+    def where(self):
+        pass
+
+    def limit(self):
+        pass
+
+    def select(self):
+        pass
+
+    def select(self):
+        pass
