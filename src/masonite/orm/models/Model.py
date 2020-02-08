90bc63d35e5a33edcc09723b30ad28ed501f36dc
Joseph Mancuso <idmann509@gmail.com>
2020-02-08 00:35:51 -0500

    added more model methods

--- src/masonite/orm/models/Model.py
+++ src/masonite/orm/models/Model.py
@@ -1,6 +1,20 @@
 class Model:
+
+    __fillable__ = []
+    __guarded__ = ['*']
+    __table__ = 'users'
+    __connection__ = 'default'
     
     def first(self):
+        pass
+    
+    def find(self):
+        pass
+    
+    def first_or_new(self):
+        pass
+    
+    def first_or_create(self):
         pass
 
     def where(self):
@@ -12,5 +26,23 @@ class Model:
     def select(self):
         pass
 
-    def select(self):
+    def hydrate(self):
+        pass
+
+    def fill(self):
+        pass
+
+    def create(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def get(self):
+        pass
+
+    def find_or_fail(self):
+        pass
+
+    def update_or_create(self):
         pass
