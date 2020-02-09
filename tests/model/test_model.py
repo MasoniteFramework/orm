170fd1f5f8656192d6534ce17c3af76cee712fa0
Joseph Mancuso <idmann509@gmail.com>
2020-02-08 22:13:02 -0500

    added optional run for model

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -1,16 +1,16 @@
 import unittest
 from src.masonite.orm.grammer.mssql_grammer import MSSQLGrammer
 from app.User import User
+import os
 
+if os.getenv('RUN_MYSQL_DATABASE', False):
+    class TestModel(unittest.TestCase):
 
-
-class TestModel(unittest.TestCase):
-
-    def test_can_call(self):
-        print(User.find(1))
-        print(User.find(2))
-        print(User.find(3))
-        print(User.find(4))
-        print(User.find(5))
-        print(User.find(6))
-        print(User.find(7))
+        def test_can_call(self):
+            print(User.find(1))
+            print(User.find(2))
+            print(User.find(3))
+            print(User.find(4))
+            print(User.find(5))
+            print(User.find(6))
+            print(User.find(7))
