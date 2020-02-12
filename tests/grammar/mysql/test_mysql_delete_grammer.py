5e15b30cbae56800af749c3f30e99a0a8d409486
Joseph Mancuso <idmann509@gmail.com>
2020-02-11 21:37:18 -0500

    refactored delete and insert test

--- tests/grammar/mysql/test_mysql_delete_grammer.py
+++ tests/grammar/mysql/test_mysql_delete_grammer.py
@@ -1,14 +1,12 @@
 from src.masonite.orm.grammar.mysql_grammar import MySQLGrammar 
 from src.masonite.orm.builder.QueryBuilder import QueryBuilder
 from src.masonite.orm.grammar.GrammarFactory import GrammarFactory 
-import unittest
+import unittest, inspect
 
-class TestMySQLUpdateGrammar(unittest.TestCase):
-
+class BaseDeleteGrammarTest:
 
     def setUp(self):
-        self.builder = QueryBuilder(GrammarFactory.make('mysql'), table='users')
-
+        self.builder = QueryBuilder(GrammarFactory.make(self.grammar), table='users')
 
     def test_can_compile_delete(self):
         to_sql = (
@@ -17,7 +15,7 @@ class TestMySQLUpdateGrammar(unittest.TestCase):
             .to_sql()
         )
 
-        sql = "DELETE FROM `users` WHERE `id` = '1'"
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(to_sql, sql)
 
     def test_can_compile_delete_with_where(self):
@@ -30,5 +28,32 @@ class TestMySQLUpdateGrammar(unittest.TestCase):
             .to_sql()
         )
 
-        sql = "DELETE FROM `users` WHERE `age` = '20' AND `profile` = '1'"
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(to_sql, sql)
+
+class TestMySQLDeleteGrammar(BaseDeleteGrammarTest, unittest.TestCase):
+
+    grammar = 'mysql'
+
+    def can_compile_delete(self):
+        """
+        (
+            self.builder
+            .delete('id', 1)
+            .to_sql()
+        )
+        """
+        return "DELETE FROM `users` WHERE `id` = '1'"
+
+    def can_compile_delete_with_where(self):
+        """
+        (
+            self.builder
+            .where('age', 20)
+            .where('profile', 1)
+            .set_action('delete')
+            .delete()
+            .to_sql()
+        )
+        """        
+        return "DELETE FROM `users` WHERE `age` = '20' AND `profile` = '1'"
--- tests/grammar/mysql/test_mysql_insert_grammer.py
+++ tests/grammar/mysql/test_mysql_insert_grammer.py
@@ -2,11 +2,12 @@ from src.masonite.orm.grammar.mysql_grammar import MySQLGrammar
 from src.masonite.orm.builder.QueryBuilder import QueryBuilder
 from src.masonite.orm.grammar.GrammarFactory import GrammarFactory
 import unittest
+import inspect
 
-class TestMySQLUpdateGrammar(unittest.TestCase):
+class BaseInsertGrammarTest:
 
     def setUp(self):
-        self.builder = QueryBuilder(GrammarFactory.make('mysql'), table='users')
+        self.builder = QueryBuilder(GrammarFactory.make(self.grammar), table='users')
 
     def test_can_compile_insert(self):
 
@@ -14,5 +15,17 @@ class TestMySQLUpdateGrammar(unittest.TestCase):
             'name': 'Joe'
         }).to_sql()
 
-        sql = "INSERT INTO `users` (`name`) VALUES ('Joe')"
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(to_sql, sql)
+
+class TestMySQLUpdateGrammar(BaseInsertGrammarTest, unittest.TestCase):
+
+    grammar = 'mysql'
+
+    def can_compile_insert(self):
+        """
+        self.builder.create({
+            'name': 'Joe'
+        }).to_sql()
+        """
+        return "INSERT INTO `users` (`name`) VALUES ('Joe')"
