c3c817b7ae42fac12eb76e1d017b19f954a998b3
Joseph Mancuso <idmann509@gmail.com>
2020-02-11 21:23:05 -0500

    refactored qmark test

--- tests/grammar/mysql/test_mysql_qmark.py
+++ tests/grammar/mysql/test_mysql_qmark.py
@@ -2,25 +2,41 @@ from src.masonite.orm.grammar.mysql_grammar import MySQLGrammar
 from src.masonite.orm.builder.QueryBuilder import QueryBuilder
 from src.masonite.orm.grammar.GrammarFactory import GrammarFactory
 import unittest
+import inspect
 
-class TestMySQLQmark(unittest.TestCase):
+class BaseQMarkTest:
 
     def setUp(self):
         self.builder = QueryBuilder(GrammarFactory.make('mysql'), table='users')
 
     def test_can_compile_select(self):
-
         mark = self.builder.select('username').where('name', 'Joe').set_action('select')
 
-        sql = "SELECT `username` FROM `users` WHERE `name` = '?'"
+        sql, bindings = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(mark.to_qmark(), sql)
-        self.assertEqual(mark._bindings, ('Joe',))
+        self.assertEqual(mark._bindings, bindings)
 
     def test_can_compile_update(self):
         mark = self.builder.update({
             'name': 'Bob'
         }).where('name', 'Joe')
 
-        sql = "UPDATE `users` SET `name` = '?' WHERE `name` = '?'"
+        sql, bindings = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(mark.to_qmark(), sql)
-        self.assertEqual(mark._bindings, ('Bob','Joe',))
+        self.assertEqual(mark._bindings, bindings)
+
+class TestMySQLQmark(BaseQMarkTest, unittest.TestCase):
+
+    def can_compile_select(self):
+        """
+        self.builder.select('username').where('name', 'Joe').set_action('select')
+        """
+        return "SELECT `username` FROM `users` WHERE `name` = '?'", ('Joe',)
+
+    def can_compile_update(self):
+        """
+        self.builder.update({
+            'name': 'Bob'
+        }).where('name', 'Joe')
+        """
+        return "UPDATE `users` SET `name` = '?' WHERE `name` = '?'", ('Bob','Joe',)
