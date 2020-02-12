103a8d568cd2addb2c4d2e54a032fa65878f276a
Joseph Mancuso <idmann509@gmail.com>
2020-02-11 22:41:49 -0500

    refactored tests

--- tests/schema/mysql/test_mysql_schema.py
+++ tests/schema/mysql/test_mysql_schema.py
@@ -2,27 +2,25 @@ from src.masonite.orm.grammar.mysql_grammar import MySQLGrammar
 from src.masonite.orm.blueprint.Blueprint import Blueprint
 from src.masonite.orm.grammar.GrammarFactory import GrammarFactory 
 from src.masonite.orm.schema.Schema import Schema
-import unittest
+import unittest, inspect
 
-
-class TestMySQLCreateGrammar(unittest.TestCase):
+class BaseTestCreateGrammar:
 
     def setUp(self):
-        Schema.set_default_string_length('255')
         self.schema = Schema.on('mysql')
 
     def test_can_compile_column(self):
         with self.schema.create('users') as blueprint:
             blueprint.string('name')
 
-        sql = "CREATE TABLE `users` (`name` VARCHAR(255) NOT NULL)"
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_can_compile_column_constraint(self):
         with self.schema.create('users') as blueprint:
             blueprint.string('name').unique()
 
-        sql = "CREATE TABLE `users` (`name` VARCHAR(255) NOT NULL, CONSTRAINT name_unique UNIQUE (name))"
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_can_compile_multiple_columns(self):
@@ -30,24 +28,14 @@ class TestMySQLCreateGrammar(unittest.TestCase):
             blueprint.string('name').nullable()
             blueprint.integer('age')
 
-        print('query', blueprint.to_sql())
-
-        sql = ("CREATE TABLE `users` ("
-                "`name` VARCHAR(255), "
-                "`age` INT(11) NOT NULL"
-            ")"
-        )
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_can_compile_not_null(self):
         with self.schema.create('users') as blueprint:
             blueprint.string('name')
 
-        sql = ("CREATE TABLE `users` ("
-                "`name` VARCHAR(255) NOT NULL"
-            ")"
-        )
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_can_compile_primary_key(self):
@@ -55,12 +43,7 @@ class TestMySQLCreateGrammar(unittest.TestCase):
             blueprint.increments('id')
             blueprint.string('name')
 
-        sql = ("CREATE TABLE `users` ("
-                "`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
-                "`name` VARCHAR(255) NOT NULL"
-            ")"
-        )
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_can_compile_primary_key(self):
@@ -68,106 +51,47 @@ class TestMySQLCreateGrammar(unittest.TestCase):
             blueprint.increments('id')
             blueprint.string('name')
 
-        sql = ("CREATE TABLE `users` ("
-                "`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
-                "`name` VARCHAR(255) NOT NULL"
-            ")"
-        )
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
-
-    # def test_timestamps_column(self):
-    #     with self.schema.create('users') as blueprint:
-    #         blueprint.string('name')
-    #         blueprint.timestamps()
-
-    #     sql = ("CREATE TABLE `users` ("
-    #             "`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
-    #             "`name` VARCHAR(255) NOT NULL"
-    #             "`created_at` DATETIME NOT NULL, "
-    #             "`updated_at` DATETIME NOT NULL"
-    #         ")"
-    #     )
-
-    #     self.assertEqual(blueprint.to_sql(), sql)
             
     def test_can_compile_multiple_constraints(self):
         with self.schema.create('users') as blueprint:
             blueprint.increments('id')
             blueprint.string('name').unique()
 
-        sql = (
-            "CREATE TABLE `users` ("
-                "`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
-                "`name` VARCHAR(255) NOT NULL, "
-                "CONSTRAINT name_unique UNIQUE (name)"
-            ")"
-        )
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_can_compile_enum(self):
         with self.schema.create('users') as blueprint:
             blueprint.enum('age', [1,2,3]).nullable()
 
-        sql = ("CREATE TABLE `users` ("
-                "`age` ENUM('1','2','3')"
-            ")"
-        )
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_column_exists(self):
         to_sql = self.schema.has_column('users', 'email', query_only=True)
 
-        sql = "SHOW COLUMNS FROM `users` LIKE 'email'"
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(to_sql, sql)
-
 
     def test_drop_table(self):
         to_sql = self.schema.drop_table('users', query_only=True)
 
-        sql = "DROP TABLE `users`"
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(to_sql, sql)
 
-    def test_drop_table(self):
+    def test_drop_table_if_exists(self):
         to_sql = self.schema.drop_table_if_exists('users', query_only=True)
 
-        sql = "DROP TABLE IF EXISTS `users`"
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(to_sql, sql)
 
     def test_drop_column(self):
         with self.schema.table('users') as blueprint:
             blueprint.drop_column('name')
 
-        sql = ("ALTER TABLE `users` "
-                "DROP COLUMN `name`")
-
-        self.assertEqual(blueprint.to_sql(), sql)
-
-    def test_default_string_length(self):
-        with self.schema.table('users') as blueprint:
-            blueprint.string('name')
-        
-        self.assertEqual(str(blueprint._columns[0].length), '255')
-
-        sql = ("ALTER TABLE `users` "
-                "ADD `name` VARCHAR(255) NOT NULL")
-
-        self.assertEqual(blueprint.to_sql(), sql)
-        
-        Schema.set_default_string_length('191')
-
-        with self.schema.table('users') as blueprint:
-            blueprint.string('name')
-        
-        self.assertEqual(str(blueprint._columns[0].length), '191')
-
-        sql = ("ALTER TABLE `users` "
-                "ADD `name` VARCHAR(191) NOT NULL")
-
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
         self.assertEqual(blueprint.to_sql(), sql)
 
     def test_can_compile_large_blueprint(self):
@@ -186,7 +110,161 @@ class TestMySQLCreateGrammar(unittest.TestCase):
             blueprint.text('description')
             blueprint.unsigned('bank').nullable()
 
-        sql = ("CREATE TABLE `users` ("
+        sql = getattr(self, inspect.currentframe().f_code.co_name.replace('test_', ''))()
+        self.assertEqual(blueprint.to_sql(), sql)
+
+
+class TestMySQLCreateGrammar(BaseTestCreateGrammar, unittest.TestCase):
+
+    def setUp(self):
+        self.schema = Schema.on('mysql')
+
+    def can_compile_column(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.string('name')
+        """
+
+        return "CREATE TABLE `users` (`name` VARCHAR(255) NOT NULL)"
+
+    def can_compile_column_constraint(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.string('name').unique()
+        """
+
+        return "CREATE TABLE `users` (`name` VARCHAR(255) NOT NULL, CONSTRAINT name_unique UNIQUE (name))"
+
+    def can_compile_multiple_columns(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.string('name').nullable()
+            blueprint.integer('age')
+        """
+
+        return (
+            "CREATE TABLE `users` ("
+                "`name` VARCHAR(255), "
+                "`age` INT(11) NOT NULL"
+            ")"
+        )
+
+    def can_compile_not_null(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.string('name')
+        """
+
+        return (
+            "CREATE TABLE `users` ("
+                "`name` VARCHAR(255) NOT NULL"
+            ")"
+        )
+
+    def can_compile_primary_key(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.increments('id')
+            blueprint.string('name')
+        """
+
+        return (
+            "CREATE TABLE `users` ("
+                "`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
+                "`name` VARCHAR(255) NOT NULL"
+            ")"
+        )
+
+    def can_compile_primary_key(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.increments('id')
+            blueprint.string('name')
+        """
+
+        return (
+            "CREATE TABLE `users` ("
+                "`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
+                "`name` VARCHAR(255) NOT NULL"
+            ")"
+        )
+            
+    def can_compile_multiple_constraints(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.increments('id')
+            blueprint.string('name').unique()
+        """
+
+        return (
+            "CREATE TABLE `users` ("
+                "`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, "
+                "`name` VARCHAR(255) NOT NULL, "
+                "CONSTRAINT name_unique UNIQUE (name)"
+            ")"
+        )
+
+    def can_compile_enum(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.enum('age', [1,2,3]).nullable()
+        """
+
+        return (
+            "CREATE TABLE `users` ("
+                "`age` ENUM('1','2','3')"
+            ")"
+        )
+
+    def column_exists(self):
+        """
+        self.schema.has_column('users', 'email', query_only=True)
+        """
+
+        return "SHOW COLUMNS FROM `users` LIKE 'email'"
+
+    def drop_table(self):
+        """
+        to_sql = self.schema.drop_table('users', query_only=True)
+        """
+
+        return "DROP TABLE `users`"
+
+    def drop_table_if_exists(self):
+        """
+        to_sql = self.schema.drop_table_if_exists('users', query_only=True)
+        """
+
+        return "DROP TABLE IF EXISTS `users`"
+
+    def drop_column(self):
+        """
+        with self.schema.table('users') as blueprint:
+            blueprint.drop_column('name')
+        """
+
+        return ("ALTER TABLE `users` "
+                "DROP COLUMN `name`")
+
+    def can_compile_large_blueprint(self):
+        """
+        with self.schema.create('users') as blueprint:
+            blueprint.string('name')
+            blueprint.string('email')
+            blueprint.string('password')
+            blueprint.integer('age').nullable()
+            blueprint.enum('type', ['Open', 'Closed'])
+            blueprint.datetime('pick_up')
+            blueprint.binary('profile')
+            blueprint.boolean('of_age')
+            blueprint.char('first_initial', length=4)
+            blueprint.date('birthday')
+            blueprint.decimal('credit', 17,6)
+            blueprint.text('description')
+            blueprint.unsigned('bank').nullable()
+        """
+
+        return ("CREATE TABLE `users` ("
                 "`name` VARCHAR(255) NOT NULL, "
                 "`email` VARCHAR(255) NOT NULL, "
                 "`password` VARCHAR(255) NOT NULL, "
@@ -202,5 +280,26 @@ class TestMySQLCreateGrammar(unittest.TestCase):
                 "`bank` INT UNSIGNED"
             ")"
         )
-        print('query', blueprint.to_sql())
+
+    def test_default_string_length(self):
+        with self.schema.table('users') as blueprint:
+            blueprint.string('name')
+        
+        self.assertEqual(str(blueprint._columns[0].length), '255')
+
+        return ("ALTER TABLE `users` "
+                "ADD `name` VARCHAR(255) NOT NULL")
+
+        self.assertEqual(blueprint.to_sql(), sql)
+        
+        Schema.set_default_string_length('191')
+
+        with self.schema.table('users') as blueprint:
+            blueprint.string('name')
+        
+        self.assertEqual(str(blueprint._columns[0].length), '191')
+
+        return ("ALTER TABLE `users` "
+                "ADD `name` VARCHAR(191) NOT NULL")
+
         self.assertEqual(blueprint.to_sql(), sql)
