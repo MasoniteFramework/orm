d41ee0aa1262fede332ad76daef9f93a118c344d
Joseph Mancuso <idmann509@gmail.com>
2020-02-27 20:10:40 -0500

    added test collection

--- src/masonite/orm/collection/Collection.py
+++ src/masonite/orm/collection/Collection.py
@@ -1,2 +1,130 @@
 class Collection:
-    pass
+
+    def __init__(self, items=[]):
+        self.items = items
+    
+    def take(self, number):
+        return self.items[:number]
+    
+    def first(self):
+        return self.items[0]
+    
+    def last(self):
+        return self.items[-1]
+
+    def all(self):
+        pass
+
+    def avg(self):
+        pass
+
+    def chunk(self):
+        pass
+
+    def collapse(self):
+        pass
+
+    def contains(self):
+        pass
+
+    def count(self):
+        pass
+
+    def diff(self):
+        pass
+
+    def each(self):
+        pass
+
+    def every(self):
+        pass
+
+    def filter(self):
+        pass
+
+    def first(self):
+        pass
+
+    def flatten(self):
+        pass
+
+    def forget(self):
+        pass
+
+    def for_page(self):
+        pass
+
+    def get(self):
+        pass
+
+    def implode(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def last(self):
+        pass
+
+    def map(self):
+        pass
+
+    def merge(self):
+        pass
+
+    def pluck(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def prepend(self):
+        pass
+
+    def pull(self):
+        pass
+
+    def push(self):
+        pass
+
+    def put(self):
+        pass
+
+    def reduce(self):
+        pass
+
+    def reject(self):
+        pass
+
+    def reverse(self):
+        pass
+
+    def serialize(self):
+        pass
+
+    def shift(self):
+        pass
+
+    def sort(self):
+        pass
+
+    def sum(self):
+        pass
+
+    def take(self):
+        pass
+
+    def to_json(self):
+        pass
+
+    def transform(self):
+        pass
+
+    def unique(self):
+        pass
+
+    def where(self):
+        pass
+
+    def zip(self):
+        pass
---
+++ tests/collection/test_collection.py
@@ -0,0 +1,15 @@
+from src.masonite.orm.collection.Collection import Collection
+import unittest
+class TestCollection(unittest.TestCase):
+
+    def test_take(self):
+        collection = Collection([1,2,3,4])
+        self.assertEqual(collection.take(2), [1,2])
+
+    def test_first(self):
+        collection = Collection([1,2,3,4])
+        self.assertEqual(collection.first(), 1)
+
+    def test_last(self):
+        collection = Collection([1,2,3,4])
+        self.assertEqual(collection.last(), 4)
