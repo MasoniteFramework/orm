40144c3d2010406ea5a0df4b6c82da3fa6df85dc
Joseph Mancuso <idmann509@gmail.com>
2020-02-29 21:59:20 -0500

    added comments

--- src/masonite/orm/relationships/BelongsTo.py
+++ src/masonite/orm/relationships/BelongsTo.py
@@ -60,4 +60,15 @@ class BelongsTo:
         )
 
     def apply_query(self, foreign, owner, foreign_key, local_key):
+        """Apply the query and return a dictionary to be hydrated
+        
+        Arguments:
+            foreign {oject} -- The relationship object
+            owner {object} -- The current model oject.
+            foreign_key {string} -- The column to access on the relationship model.
+            local_key {string} -- The column to access on the current model.
+        
+        Returns:
+            dict -- A dictionary of data which will be hydrated.
+        """        
         return foreign.where(foreign_key, owner.__attributes__[local_key]).first()
