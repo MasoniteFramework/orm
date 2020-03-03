d169eba5e33a8cec8eee73c25dedd5aa05e8b432
Marlysson <marlysson5@gmail.com>
2020-03-03 02:11:03 -0300

    Adding tests to verify some basics data of tables and columns related to foreign key.

--- tests/schema/mysql/test_mysql_foreign_key_constraint.py
+++ tests/schema/mysql/test_mysql_foreign_key_constraint.py
@@ -1,9 +1,34 @@
+from src.masonite.orm.schema.Schema import Schema
 from unittest import TestCase
 
-class BaseTestCreateForeignKeyGrammar:
+class BaseTestCreateForeignKeyGrammar(TestCase):
 
     def setUp(self):
         self.schema = Schema.on('mysql')
+
+    def test_quantity_of_relations(self):
+        with self.schema.create('users') as table:
+            table.foreign('user_id').references('id').on('users')
+            table.foreign('profile_id').references('id').on('profile')
+            table.foreign('company_id').references('id').on('company')
+
+        self.assertEqual(len(table._relations), 3)
+
+    def test_correct_data_of_foreign_key_when_defined_more_than_one_relation(self):
+        with self.schema.create('users') as table:
+            table.foreign('user_id').references('id').on('users')
+            table.foreign('profile_id').references('id').on('profile')
+
+        first_relation = table._relations[0]
+        second_relation = table._relations[1]
+
+        self.assertEqual(first_relation.get("local_column"), "user_id")
+        self.assertEqual(first_relation.get("external_column"), "id")
+        self.assertEqual(first_relation.get("table"), "users")
+
+        self.assertEqual(second_relation.get("local_column"), "profile_id")
+        self.assertEqual(second_relation.get("external_column"), "id")
+        self.assertEqual(second_relation.get("table"), "profile")
 
     def test_throw_exception_when_foreign_key_column_doesnt_exist(self):
         pass
@@ -14,10 +39,10 @@ class BaseTestCreateForeignKeyGrammar:
     def test_throw_exception_when_foreign_key_table_doesnt_exist(self):
         pass
 
-class TestMySQLForeignKeyGrammar(BaseTestCreateForeignKeyGrammar, TestCase):
-    with self.schema.create('users') as table:
-        table.increments('id')
-        table.string('name')
+# class TestMySQLForeignKeyGrammar(BaseTestCreateForeignKeyGrammar):
+#     with self.schema.create('users') as table:
+#         table.increments('id')
+#         table.string('name')
 
-        table.foreign('profile_id').references('user').on('users')
+#         table.foreign('profile_id').references('user').on('users')
 
