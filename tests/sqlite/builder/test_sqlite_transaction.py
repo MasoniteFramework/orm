1078310bd9a9ab6d5d7fca541dfa8af0802a9945
Joe Mancuso <idmann509@gmail.com>
2020-09-27 17:08:06 -0400

    added sqlite transaciton test

---
+++ tests/sqlite/builder/test_sqlite_transaction.py
@@ -0,0 +1,40 @@
+import inspect
+import unittest
+
+from src.masoniteorm.orm.query import QueryBuilder
+from src.masoniteorm.orm.query.grammars import SQLiteGrammar
+from src.masoniteorm.orm.connections import ConnectionFactory
+from src.masoniteorm.orm.relationships import belongs_to
+from src.masoniteorm.orm.models import Model
+from tests.utils import MockConnectionFactory
+from config.database import DATABASES
+
+
+class User(Model):
+    __connection__ = "sqlite"
+    __timestamps__ = False
+
+
+class BaseTestQueryRelationships(unittest.TestCase):
+
+    maxDiff = None
+
+    def get_builder(self, table="users"):
+        connection = ConnectionFactory().make("sqlite")
+        return QueryBuilder(
+            grammar=SQLiteGrammar,
+            connection=connection,
+            table=table,
+            # model=User,
+            connection_details=DATABASES,
+        ).on("sqlite")
+
+    def test_transaction(self):
+        builder = self.get_builder()
+        builder.begin()
+        builder.create({"name": "phillip3", "email": "phillip3"})
+        user = builder.where("name", "phillip3").first()
+        self.assertEqual(user["name"], "phillip3")
+        builder.rollback()
+        user = builder.where("name", "phillip3").first()
+        self.assertEqual(user, None)
