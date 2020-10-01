90b40eda3cad708b5b70ad432be96b176d7e579f
Samuel Girardin <samuelgirardin@pm.me>
2020-10-01 17:38:28 +0200

    add ORMProvider

---
+++ src/masoniteorm/providers/ORMProvider.py
@@ -0,0 +1,31 @@
+from masonite.provider import ServiceProvider
+from masonite.helpers import config
+
+from masoniteorm.connections import ConnectionResolver
+from masoniteorm.commands import (
+    MakeMigrationCommand,
+    MakeSeedCommand,
+    MigrateCommand,
+    MigrateRefreshCommand,
+    MigrateRollbackCommand,
+)
+
+
+class ORMProvider(ServiceProvider):
+    """Masonite ORM database provider to be used inside
+    Masonite based projects."""
+
+    wsgi = False
+
+    def register(self):
+        ConnectionResolver.set_connection_details(config("database.databases"))
+        self.commands(
+            MakeMigrationCommand(),
+            MakeSeedCommand(),
+            MigrateCommand(),
+            MigrateRefreshCommand(),
+            MigrateRollbackCommand(),
+        )
+
+    def boot(self):
+        pass
