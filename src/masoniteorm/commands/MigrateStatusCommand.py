7339a133023a1cd7d7c3a149e25c36392d49288e
Joe Mancuso <idmann509@gmail.com>
2020-10-11 19:21:07 -0400

    added migrate status command

---
+++ src/masoniteorm/commands/MigrateStatusCommand.py
@@ -0,0 +1,34 @@
+from cleo import Command
+import os
+from ..migrations import Migration
+
+
+class MigrateStatusCommand(Command):
+    """
+    Run migrations.
+
+    migrate:status
+        {--c|connection=default : The connection you want to run migrations on}
+        {--f|force : Force migrations without prompt in production}
+    """
+
+    def handle(self):
+        from config.database import ConnectionResolver
+
+        # prompt user for confirmation in production
+
+        migration = Migration(command_class=self, connection=self.option("connection"))
+        migration.create_table_if_not_exists()
+        table = self.table()
+        table.set_header_row(["Ran?", "Migration"])
+        migrations = []
+
+        for migration in migration.get_ran_migrations():
+            migrations.append(["<info>Y</info>", f"<comment>{migration}</comment>"])
+
+        for migration in migration.get_unran_migrations():
+            migrations.append(["<error>N</error>", f"<comment>{migration}</comment>"])
+
+        table.set_rows(migrations)
+
+        table.render(self.io)
