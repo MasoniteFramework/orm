f628169e21c779e68fb56310ee013183f0c49e0c
Samuel Girardin <samuelgirardin@pm.me>
2020-10-20 09:18:11 +0200

    fix mixin

--- src/masoniteorm/scopes/UUIDPrimaryKeyMixin.py
+++ src/masoniteorm/scopes/UUIDPrimaryKeyMixin.py
@@ -4,5 +4,5 @@ from .UUIDPrimaryKeyScope import UUIDPrimaryKeyScope
 class UUIDPrimaryKeyMixin:
     """Global scope class to add UUID as primary key to models."""
 
-    def boot_TimeStampsMixin(self, builder):
+    def boot_UUIDPrimaryKeyMixin(self, builder):
         builder.set_global_scope(UUIDPrimaryKeyScope())
