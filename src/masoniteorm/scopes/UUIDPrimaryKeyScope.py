27b4aed83fb201e6a49c6294883bedc07b577e12
Samuel Girardin <samuelgirardin@pm.me>
2020-10-20 11:28:44 +0200

    fix uuid scope with version 3 and 5

--- src/masoniteorm/scopes/UUIDPrimaryKeyScope.py
+++ src/masoniteorm/scopes/UUIDPrimaryKeyScope.py
@@ -7,17 +7,25 @@ class UUIDPrimaryKeyScope(BaseScope):
 
     def on_boot(self, builder):
         builder.set_global_scope(
-            "_uuid", self.set_uuid_create, action="insert"
+            "_UUID_primary_key", self.set_uuid_create, action="insert"
         )
 
     def on_remove(self, builder):
         pass
 
+    def generate_uuid(self, builder, uuid_version):
+        # UUID 3 and 5 requires parameters
+        uuid_func = getattr(uuid, f"uuid{uuid_version}")
+        args = []
+        if uuid_version in [3, 5]:
+            args = [builder._model.__uuid_namespace__, builder._model.__uuid_name__]
+
+        return str(uuid_func(*args))
+
     def set_uuid_create(self, builder):
-        uuid_version = builder._model.__uuid_version__ or 4
-        uuid_generator = getattr(uuid, f"uuid{uuid_version}")
+        uuid_version = getattr(builder._model, "__uuid_version__", 4)
         builder._creates.update(
             {
-                builder._model.__primary_key__: str(uuid_generator()),
+                builder._model.__primary_key__: self.generate_uuid(builder, uuid_version),
             }
         )
