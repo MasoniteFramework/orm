8da32c5e59d3704d6bcfdcb3a51c59166fdb72b5
Samuel Girardin <samuelgirardin@pm.me>
2020-10-31 23:39:00 +0100

    update transaction class to handle both decorator or context manager use

--- src/masoniteorm/connections/decorators.py
+++ src/masoniteorm/connections/decorators.py
@@ -1,17 +1,30 @@
+from contextlib import ContextDecorator
 from config.database import db
 
 
-class Transaction(object):
+class Transaction(ContextDecorator):
 
-    def __init__(self, connection="default"):
+    def __init__(self, using, connection="default"):
+        self.using = using
         self.connection = connection
 
-    def __call__(self, fn, *args, **kwargs):
-        # before function
-        db.begin_transaction(self.connection)
-        try:
-            return fn
-        except:
+    def __enter__(self):
+        connection = db.begin_transaction(self.connection)
+        return connection
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_value:
             db.rollback(self.connection)
-        finally:
+        else:
             db.commit(self.connection)
+        return True
+
+
+def transaction(using=None, connection="default"):
+    """Function allowing to use the class as a context manager or a decorator.
+    @transaction(...) or context manager: with transaction(...)
+    """
+    if callable(using):
+        return Transaction(connection)(using)
+    else:
+        return Transaction(connection, using)
--- tests/sqlite/builder/test_sqlite_transaction.py
+++ tests/sqlite/builder/test_sqlite_transaction.py
@@ -1,14 +1,11 @@
-import inspect
 import unittest
 
 from config.database import DATABASES
 from src.masoniteorm.connections import ConnectionFactory
-from src.masoniteorm.connections.decorators import Transaction as transaction
+from src.masoniteorm.connections.decorators import transaction
 from src.masoniteorm.models import Model
 from src.masoniteorm.query import QueryBuilder
 from src.masoniteorm.query.grammars import SQLiteGrammar
-from src.masoniteorm.relationships import belongs_to
-from tests.utils import MockConnectionFactory
 from config.database import db
 from src.masoniteorm.collection import Collection
 
@@ -17,16 +14,6 @@ class User(Model):
     __connection__ = "sqlite"
     __timestamps__ = False
 
-
-@transaction(connection="sqlite")
-def create_user():
-    User.create({"name": "phillip2", "email": "phillip3"})
-
-@transaction(connection="sqlite")
-def create_user_with_error():
-    User.create({"name": "phillip3", "email": "phillip3"})
-    raise Exception("error faking")
-    User.create({"name": "phillip4", "email": "phillip4"})
 
 
 class BaseTestQueryRelationships(unittest.TestCase):
@@ -53,24 +40,25 @@ class BaseTestQueryRelationships(unittest.TestCase):
         user = builder.where("name", "phillip3").first()
         self.assertEqual(user, None)
 
+    def test_transaction_with_context_manager(self):
+        with transaction("sqlite") as builder:
+            builder.create({"name": "sam", "email": "sam"})
+        assert User.where("name", "sam").get().count() == 1
+
+    def test_transaction_rollback_with_context_manager(self):
+        with transaction("sqlite") as builder:
+            builder.create({"name": "sam", "email": "sam"})
+            assert User.where("name", "sam").get().count() == 1
+            raise Exception("simulate error")
+
+        assert User.where("name", "sam").get().count() == 0
+
     def test_transaction_globally(self):
         connection = db.begin_transaction("sqlite")
         self.assertEqual(connection, self.get_builder().new_connection())
         db.commit("sqlite")
         db.begin_transaction("sqlite")
         db.rollback("sqlite")
-
-    def test_transaction_if_sucess(self):
-        count = User.all().count()
-        create_user()
-        assert User.all().count() == count + 1
-
-    def test_transaction_rollback_if_error(self):
-        count = User.all().count()
-        with self.assertRaises(Exception):
-            create_user_with_error()
-        # check no users created
-        assert User.all().count() == count
 
     def test_chunking(self):
         for users in self.get_builder().chunk(10):
