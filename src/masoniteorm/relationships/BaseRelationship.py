cb46fffad429ded34230fc206c4d2726a29d4cde
Joe Mancuso <idmann509@gmail.com>
2021-01-23 12:36:48 -0500

    put back removed relationship

--- src/masoniteorm/relationships/BaseRelationship.py
+++ src/masoniteorm/relationships/BaseRelationship.py
@@ -67,17 +67,16 @@ class BaseRelationship:
         relationship = self.fn(self)()
         return getattr(relationship.builder, attribute)
 
-    # def apply_query(self, foreign, owner, foreign_key, local_key):
-    #     """Apply the query and return a dictionary to be hydrated
+    def apply_query(self, foreign, owner, foreign_key, local_key):
+        """Apply the query and return a dictionary to be hydrated
 
-    #     Arguments:
-    #         foreign {oject} -- The relationship object
-    #         owner {object} -- The current model oject.
-    #         foreign_key {string} -- The column to access on the relationship model.
-    #         local_key {string} -- The column to access on the current model.
+        Arguments:
+            foreign {oject} -- The relationship object
+            owner {object} -- The current model oject.
+            foreign_key {string} -- The column to access on the relationship model.
+            local_key {string} -- The column to access on the current model.
 
-    #     Returns:
-    #         dict -- A dictionary of data which will be hydrated.
-    #     """
-    #     print('3')
-    #     return foreign.where(foreign_key, owner().__attributes__[local_key]).first()
+        Returns:
+            dict -- A dictionary of data which will be hydrated.
+        """
+        return foreign.where(foreign_key, owner().__attributes__[local_key]).first()
