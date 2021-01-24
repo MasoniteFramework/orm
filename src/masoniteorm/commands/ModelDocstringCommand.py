e77417bbe5e9b3dc98b7b7ecc5cd67701979216d
Marlysson <marlysson5@gmail.com>
2021-01-23 22:09:16 -0300

    Creating the command to generate docstring to Model fields.

---
+++ src/masoniteorm/commands/ModelDocstringCommand.py
@@ -0,0 +1,27 @@
+from cleo import Command
+
+class ModelDocstringCommand(Command):
+    """
+    Generate a model docstring based on a table definition
+    model:docstring
+        {table : Name of the table to generate the docstring for}
+        {--c|connection=default : The connection to use}
+    """
+
+    def handle(self):
+        from config.database import DB
+
+        if self.option("connection") == "default":
+            table_information = DB.get_schema_builder().list_table_columns(self.argument("table"))
+
+        else:
+            table_information = (
+                DB.get_schema_builder(self.option("connection"))
+                  .list_table_columns(self.argument("table"))
+            )
+
+        docstring = '"""Model Definition (generated with love by Masonite) \n\n'
+        for column_information in table_information:
+            docstring += column_information + "\n"
+
+        print(docstring + '"""')
