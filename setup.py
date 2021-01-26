b778843bbd78d92b2d76748ebb2c5a99764eb034
Joe Mancuso <idmann509@gmail.com>
2021-01-25 20:27:44 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.7",
+    version="1.0.8",
     package_dir={"": "src"},
     description="The Official Masonite ORM",
     long_description=long_description,
