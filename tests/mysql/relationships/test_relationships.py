7f58896e1921f298e3ba47753b84c641d06f7023
Joe Mancuso <idmann509@gmail.com>
2021-03-11 22:04:29 -0500

    added joins on and joins tests

--- tests/mysql/relationships/test_relationships.py
+++ tests/mysql/relationships/test_relationships.py
@@ -14,10 +14,28 @@ class Profile(Model):
 
 
 class MySQLRelationships(unittest.TestCase):
+
     def test_relationship_keys(self):
         sql = User.has("profile").to_sql()
         print(sql)
         self.assertEqual(
             sql,
             """SELECT * FROM `users` WHERE EXISTS (SELECT * FROM `profiles` WHERE `profiles`.`profile_id` = `users`.`id`)""",
+        )
+
+    def test_joins(self):
+        sql = User.joins("profile").to_sql()
+        self.assertEqual(
+            sql,
+            """SELECT * FROM `users` INNER JOIN `profiles` ON `users`.`id` = `profiles`.`profile_id`""",
+        )
+
+    def test_join_on(self):
+        sql = User.join_on("profile", lambda q: (
+            q.where("active", 1)
+        )).to_sql()
+
+        self.assertEqual(
+            sql,
+            """SELECT * FROM `users` INNER JOIN `profiles` ON `users`.`id` = `profiles`.`profile_id` WHERE (`profiles`.`active` = '1')""",
         )
