7b49308645b4ce8e398c4fa02986ee9ef99b357f
Marlysson Silva <marlysson5@gmail.com>
2021-04-09 21:10:44 -0300

    Change the api for seed after migrate:refresh to use just "--seed" or --"seed Seeder"

--- src/masoniteorm/commands/MigrateRefreshCommand.py
+++ src/masoniteorm/commands/MigrateRefreshCommand.py
@@ -9,17 +9,22 @@ class MigrateRefreshCommand(Command):
     migrate:refresh
         {--c|connection=default : The connection you want to run migrations on}
         {--d|directory=databases/migrations : The location of the migration directory}
-        {--s|seed : Wheter seeding should be run after refresh migrations}
-        {--seeder=None : Which seeder should be run}
+        {--s|seed=? : Wheter seeding should be run after refresh migrations and which seedeer should be ran}
     """
 
     def handle(self):
+
         migration = Migration(
             command_class=self,
             connection=self.option("connection"),
             migration_directory=self.option("directory"),
         )
+
         migration.refresh()
 
-        if self.option("seed"):
-            self.call("seed:run", self.option("seeder"))
+        self.line("")
+
+        if self.option("seed") == "null":
+            self.call("seed:run", "None")
+        elif self.option("seed"):
+            self.call("seed:run", self.option("seed"))
