45509db21e4e1c8d04e7e3d57b064980826d79e5
Joe Mancuso <idmann509@gmail.com>
2021-06-16 22:55:10 -0400

    changed to extra_fields

--- src/masoniteorm/relationships/BelongsToMany.py
+++ src/masoniteorm/relationships/BelongsToMany.py
@@ -18,7 +18,7 @@ class BelongsToMany(BaseRelationship):
         with_timestamps=False,
         pivot_id="id",
         attribute="pivot",
-        extra_fields=[],
+        with_fields=[],
     ):
         if isinstance(fn, str):
             self.fn = None
@@ -37,7 +37,7 @@ class BelongsToMany(BaseRelationship):
         self.with_timestamps = with_timestamps
         self._as = attribute
         self.pivot_id = pivot_id
-        self.extra_fields = extra_fields
+        self.with_fields = with_fields
 
     def set_keys(self, owner, attribute):
         self.local_foreign_key = self.local_foreign_key or "id"
@@ -105,8 +105,8 @@ class BelongsToMany(BaseRelationship):
                 f"{table1}.{self.local_owner_key}", getattr(owner, self.local_owner_key)
             )
 
-        if self.extra_fields:
-            for field in self.extra_fields:
+        if self.with_fields:
+            for field in self.with_fields:
                 result.select(f"{self._table}.{field}")
 
         result = result.get()
@@ -129,8 +129,8 @@ class BelongsToMany(BaseRelationship):
                     }
                 )
 
-            if self.extra_fields:
-                for field in self.extra_fields:
+            if self.with_fields:
+                for field in self.with_fields:
                     pivot_data.update({field: getattr(p, field)})
                     p.delete_attribute(field)
 
@@ -176,8 +176,8 @@ class BelongsToMany(BaseRelationship):
             f"{self._table}.{self.other_foreign_key}",
         ).table(f"{table1}")
 
-        if self.extra_fields:
-            for field in self.extra_fields:
+        if self.with_fields:
+            for field in self.with_fields:
                 result.select(f"{self._table}.{field}")
 
         result.join(
@@ -230,8 +230,8 @@ class BelongsToMany(BaseRelationship):
             if self.pivot_id:
                 pivot_data.update({self.pivot_id: getattr(model, self.pivot_id)})
 
-            if self.extra_fields:
-                for field in self.extra_fields:
+            if self.with_fields:
+                for field in self.with_fields:
                     pivot_data.update({field: getattr(model, field)})
                     model.delete_attribute(field)
 
