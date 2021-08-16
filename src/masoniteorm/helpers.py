080465a168b4a2c66fb6be5b10c8ae38304577a7
Kieren Eaton <circuloned@gmail.com>
2021-08-16 10:59:54 +0800

    Blacked helpers linting issues

--- src/masoniteorm/helpers.py
+++ src/masoniteorm/helpers.py
@@ -1,20 +1,25 @@
 import re
 
+
 def database_url(url):
-    regex = re.compile("(?P<schema>.*?)://(?P<user>.*?):(?P<password>.*?)@(?P<host>.*?)/(?P<database>.*)")
+    regex = re.compile(
+        "(?P<schema>.*?)://(?P<user>.*?):(?P<password>.*?)@(?P<host>.*?)/(?P<database>.*)"
+    )
     dic = {}
     match = regex.match(url)
     user = match.group(2)
     host = match.group(4)
-    hostname = host.split(':')[0]
-    port = None if ':' not in host else host.split(':')[1]
+    hostname = host.split(":")[0]
+    port = None if ":" not in host else host.split(":")[1]
     database = match.group(5)
-    dic.update({
-        "user": user,
-        "password": match.group(3),
-        "host": hostname,
-        "port": port,
-        "database": database,
-    })
+    dic.update(
+        {
+            "user": user,
+            "password": match.group(3),
+            "host": hostname,
+            "port": port,
+            "database": database,
+        }
+    )
 
     return dic
