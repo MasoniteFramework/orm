a1d8dd9dcb5e579c93f0b0397336d6bc481eccc6
Samuel Girardin <samuelgirardin@pm.me>
2021-10-07 19:12:09 +0200

    fix param type to be able to contain a value

--- src/masoniteorm/commands/CanOverrideConfig.py
+++ src/masoniteorm/commands/CanOverrideConfig.py
@@ -7,8 +7,10 @@ class CanOverrideConfig(Command):
         self.add_option()
 
     def add_option(self):
+        # 8 is the required flag constant in cleo
         self._config.add_option(
             "config",
             "C",
+            8,
             description="The path to the ORM configuration file. If not given DB_CONFIG_PATH env variable will be used and finally 'config.database'.",
         )
