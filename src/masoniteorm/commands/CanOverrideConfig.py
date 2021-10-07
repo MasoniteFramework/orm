7120c00ced77ae12ec8074f566146bd272023273
Samuel Girardin <samuelgirardin@pm.me>
2021-10-07 19:01:25 +0200

    update config parameter in command

--- src/masoniteorm/commands/CanOverrideConfig.py
+++ src/masoniteorm/commands/CanOverrideConfig.py
@@ -8,8 +8,8 @@ class CanOverrideConfig(Command):
 
     def add_option(self):
         self._config.add_option(
-            "option",
-            "o",
+            "config",
+            "f",
             8,
             "The path to the ORM configuration file. If not given DB_CONFIG_PATH env variable will be used and finally 'config.database'.",
             None,
