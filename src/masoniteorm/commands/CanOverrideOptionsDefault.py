e19baf8d55281e057e24f7dc57d2c09d3b5636f6
Samuel Girardin <samuelgirardin@pm.me>
2021-12-21 21:55:40 +0100

    improve comment

--- src/masoniteorm/commands/CanOverrideOptionsDefault.py
+++ src/masoniteorm/commands/CanOverrideOptionsDefault.py
@@ -14,9 +14,9 @@ class CanOverrideOptionsDefault:
         super().__init__()
         self.overriden_default = kwargs
         for option_name, option in self.config.options.items():
-            # Cleo does not authorize _ in option name but - are authorized unfortunately -
-            # cannot be used in Python variables, so this make sure that option-a will be
-            # accessible with option_a in Python
+            # Cleo does not authorize _ in option name but - are authorized and unfortunately -
+            # cannot be used in Python variables. So underscore() is called to make sure that
+            # an option like 'option-a' will be accessible with 'option_a' in kwargs
             default = self.overriden_default.get(underscore(option_name))
             if default:
                 option.set_default(default)
