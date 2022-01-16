a6435fb44687efed3e4167748a674c281009e184
Joseph Mancuso <idmann509@gmail.com>
2022-01-16 09:16:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="2.1.2",
+    version="2.1.3",
     package_dir={"": "src"},
     description="The Official Masonite ORM",
     long_description=long_description,
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Framework :: Masonite",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     # What does your project relate to?
