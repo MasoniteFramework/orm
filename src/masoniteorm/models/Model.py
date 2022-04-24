195bb1ef154bfcf8c453d03f822a4419461e2615
Jarriq Rolle <jrolle@bnbbahamas.com>
2022-04-24 04:21:09 -0400

    Added .first_or() method to Model.
    Retrieves first record matching a given criteria. If no record exists matching the given criteria, a user-defined callback function will be executed.

--- src/masoniteorm/models/Model.py
+++ src/masoniteorm/models/Model.py
@@ -134,6 +134,7 @@ class Model(TimeStampsMixin, ObservesEvents, metaclass=ModelMeta):
         "delete",
         "find_or_404",
         "find_or_fail",
+        "first_or",
         "first_or_fail",
         "first",
         "first_where",
@@ -569,6 +570,22 @@ class Model(TimeStampsMixin, ObservesEvents, metaclass=ModelMeta):
         return json.dumps(self.serialize())
 
     @classmethod
+    def first_or(cls, wheres, callback=None, *args):
+        """Execute the query and get the first result or call a callback.
+
+        Returns:
+            Record|Mixed
+        """
+        self = cls()
+        self._check_is_callable(callback)
+        record = self.where(wheres).first()
+
+        if not record:
+            return callback(*args)
+
+        return record
+
+    @classmethod
     def update_or_create(cls, wheres, updates):
         self = cls()
         record = self.where(wheres).first()
@@ -946,6 +963,13 @@ class Model(TimeStampsMixin, ObservesEvents, metaclass=ModelMeta):
             related_record.save()
 
         return related.attach_related(self, related_record)
+
+    def _check_is_callable(cls, callback, raise_exception=True):
+        if not callable(callback):
+            if not raise_exception:
+                return False
+            raise ValueError("The 'callback' should be a function")
+        return True
 
     @classmethod
     def on(cls, connection):
