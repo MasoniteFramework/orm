b065aaecc2d282ef8d2ff36a4440b6f68609792c
Joe Mancuso <idmann509@gmail.com>
2022-06-12 12:44:29 -0400

    fixed how scopes work

--- src/masoniteorm/scopes/scope.py
+++ src/masoniteorm/scopes/scope.py
@@ -3,10 +3,11 @@ class scope:
         self.fn = callback
 
     def __set_name__(self, cls, name):
-        cls._scopes.update({name: self.fn})
         self.cls = cls
+        self.scope_name = name
 
     def __call__(self, *args, **kwargs):
         instantiated = self.cls()
         builder = instantiated.get_builder()
+        builder._scopes.update({self.scope_name: self.fn})
         return self.fn(instantiated, builder, *args, **kwargs)
