c95d5ac72148d3cfa59a3f1aa4aa86735c03a7d1
Bader Almutairi <phpfalcon@gmail.com>
2022-10-04 10:58:00 +0300

    Update test_models.py

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -97,8 +97,7 @@ class TestModels(unittest.TestCase):
             }
         )
 
-        self.assertEqual(type(model.payload), str)
-        self.assertEqual(type(json.loads(model.payload)), list)
+        self.assertEqual(type(model.payload), list)
         self.assertEqual(type(model.x), int)
         self.assertEqual(type(model.f), float)
         self.assertEqual(type(model.is_vip), bool)
