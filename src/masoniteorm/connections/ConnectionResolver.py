879a9048176dcb274099a2fd976fe33881e14841
Matthew Altberg <matthewa@rogers.com>
2023-12-05 12:24:36 -0500

    Add optional config path to ConnectionResolver and ConnectionFactory

--- src/masoniteorm/connections/ConnectionResolver.py
+++ src/masoniteorm/connections/ConnectionResolver.py
@@ -6,7 +6,7 @@ class ConnectionResolver:
     _connections = {}
     _morph_map = {}
 
-    def __init__(self):
+    def __init__(self, config_path=None):
         from ..connections import (
             SQLiteConnection,
             PostgresConnection,
@@ -16,8 +16,7 @@ class ConnectionResolver:
 
         from ..connections import ConnectionFactory
 
-        self.connection_factory = ConnectionFactory()
-
+        self.connection_factory = ConnectionFactory(config_path=config_path)
         self.register(SQLiteConnection)
         self.register(PostgresConnection)
         self.register(MySQLConnection)
