11743db0ff3b14fcb723af27cd7e28a33a32f4a5
Kieren Eaton <499977+circulon@users.noreply.github.com>
2024-12-01 11:57:44 +0800

    Fixed using model default select columns correctly
    Only use model get_selects() if no other columns are specified

--- src/masoniteorm/query/QueryBuilder.py
+++ src/masoniteorm/query/QueryBuilder.py
@@ -1229,6 +1229,7 @@ class QueryBuilder(ObservesEvents):
         return self
 
     def with_count(self, relationship, callback=None):
+        self.select(*self._model.get_selects())
         return getattr(self._model, relationship).get_with_count_query(
             self, callback=callback
         )
@@ -2067,6 +2068,9 @@ class QueryBuilder(ObservesEvents):
 
         # Either _creates when creating, otherwise use columns
         columns = self._creates or self._columns
+        if not columns and not self._aggregates and self._model:
+            self.select(*self._model.get_selects())
+            columns = self._columns
 
         return self.grammar(
             columns=columns,
