86c151446154c00a20a42ea4ab6be55b1f89a1fe
Kieren Eaton <499977+circulon@users.noreply.github.com>
2025-02-22 10:17:22 +0800

    Fixed tests

--- tests/sqlite/relationships/test_sqlite_relationships.py
+++ tests/sqlite/relationships/test_sqlite_relationships.py
@@ -40,11 +40,9 @@ class Logo(Model):
 
 
 class User(Model):
-    __tablename__ = "users_rel"
+    __table__ = "users_rel"
     __connection__ = "dev"
-    _eager_loads = ()
     __timestamps__ = None
-    __dry__ = True
     __casts__ = {"is_admin": "bool"}
 
     @has_many(None, "id", "user_id")
@@ -60,13 +58,14 @@ class User(Model):
 
 
 class Store(Model):
+    __table__ = "stores"
     __connection__ = "dev"
 
-    @belongs_to_many("store_id", "product_id", "id", "id", with_timestamps=True)
+    @belongs_to_many(None, "store_id", "product_id", "id", "id", with_timestamps=True)
     def products(self):
         return Product
 
-    @belongs_to_many(None, "store_id", "product_id", "id", "id", table="product_table")
+    @belongs_to_many(None, "store_id", "product_id", "id", "id", table="product_store")
     def products_table(self):
         return Product
 
@@ -76,6 +75,7 @@ class Store(Model):
 
 
 class Product(Model):
+    __table__ = "products"
     __connection__ = "dev"
 
 
@@ -119,51 +119,54 @@ class SqliteTestRelationships(unittest.TestCase):
         cls.schema.drop_table_if_exists("products")
         with cls.schema.create("products") as table:
             table.integer("id").primary()
+            table.string("name")
 
         cls.schema.drop_table_if_exists("stores")
         with cls.schema.create("stores") as table:
             table.integer("id").primary()
+            table.string("name")
 
         cls.schema.drop_table_if_exists("product_store")
-        with cls.schema.create("stores") as table:
+        with cls.schema.create("product_store") as table:
             table.integer("id").primary()
             table.integer("product_id")
             table.integer("store_id")
+            table.timestamps()
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.schema.drop_table_if_exists("users_rel")
-    #     cls.schema.drop_table_if_exists("profiles_rel")
-    #     cls.schema.drop_table_if_exists("articles_rel")
-    #     cls.schema.drop_table_if_exists("logos_rel")
-    #     cls.schema.drop_table_if_exists("products")
-    #     cls.schema.drop_table_if_exists("stores")
-    #     cls.schema.drop_table_if_exists("product_store")
+    @classmethod
+    def tearDownClass(cls):
+        cls.schema.drop_table_if_exists("users_rel")
+        cls.schema.drop_table_if_exists("profiles_rel")
+        cls.schema.drop_table_if_exists("articles_rel")
+        cls.schema.drop_table_if_exists("logos_rel")
+        cls.schema.drop_table_if_exists("products")
+        cls.schema.drop_table_if_exists("stores")
+        cls.schema.drop_table_if_exists("product_store")
 
     def setUp(self):
         # create minimal data for each test
-        User.builder.new().bulk_create(
+        self.dev_builder.table("users_rel").bulk_create(
             [
                 {"name": "Steve", "is_admin": False},
                 {"name": "Joe", "is_admin": True},
                 {"name": "Ed", "is_admin": False},
             ]
         )
-        Profile.builder.new().bulk_create(
+        self.dev_builder.table("profiles_rel").bulk_create(
             [
                 {"user_id": 1, "occupation": "Scientist"},
                 {"user_id": 2, "occupation": "Developer"},
                 {"user_id": 3, "occupation": "Politician"},
             ]
         )
-        Logo.builder.new().bulk_create(
+        self.dev_builder.table("Logos_rel").bulk_create(
             [
                 {"name": "Bing"},
                 {"name": "Zap"},
                 {"name": "Swoosh"},
             ]
         )
-        Article.builder.new().bulk_create(
+        self.dev_builder.table("articles_rel").bulk_create(
             [
                 {
                     "user_id": 2,
@@ -174,7 +177,7 @@ class SqliteTestRelationships(unittest.TestCase):
             ]
         )
 
-        Product.builder.new().bulk_create(
+        self.dev_builder.table("products").bulk_create(
             [
                 {"name": "Pants"},
                 {"name": "Shirt"},
@@ -183,7 +186,7 @@ class SqliteTestRelationships(unittest.TestCase):
                 {"name": "Plate"},
             ]
         )
-        Store.builder.new().bulk_create(
+        self.dev_builder.table("stores").bulk_create(
             [
                 {"name": "K-Mart"},
                 {"name": "Target"},
@@ -191,8 +194,7 @@ class SqliteTestRelationships(unittest.TestCase):
                 {"name": "Walmart"},
             ]
         )
-        builder = QueryBuilder().on("dev")
-        builder.table("product_store").bulk_create(
+        self.dev_builder.table("product_store").bulk_create(
             [
                 {"product_id": 1, "store_id": 2},
                 {"product_id": 2, "store_id": 3},
@@ -235,13 +237,13 @@ class SqliteTestRelationships(unittest.TestCase):
         self.assertEqual(len(user.articles), 1)
 
     # def test_can_access_relationship_date(self):
-    #     user = User.with_("articles_rel").where("id", 1).first()
+    #     user = User.with_("articles").where("id", 1).first()
     #     for article in user.articles:
     #         print(article.logo.published_date.is_past())
     #
 
     # def test_loading(self):
-    #     users = User.with_("articles_rel").get()
+    #     users = User.with_("articles").get()
     #     for user in users:
     #         user
 
@@ -250,18 +252,18 @@ class SqliteTestRelationships(unittest.TestCase):
         self.assertEqual(User.profile().to_sql(), sql)
 
     def test_loading_with_nested_with(self):
-        users = User.with_("articles_rel", "articles.logo").get()
+        users = User.with_("articles", "articles.logo").get()
         for user in users:
             for article in user.articles:
                 self.assertIsNotNone(article.logo)
 
     def test_casting(self):
-        users = User.with_("articles_rel").where("is_admin", True).get()
+        users = User.with_("articles").where("is_admin", True).get()
         for user in users:
             self.assertIsInstance(user.is_admin, bool)
 
     # def test_setting(self):
-    #     users = User.with_("articles_rel").where("is_admin", True).get()
+    #     users = User.with_("articles").where("is_admin", True).get()
     #     for user in users:
     #         user.name = "Joe"
     #         user.is_admin = 1
@@ -278,9 +280,9 @@ class SqliteTestRelationships(unittest.TestCase):
     def test_associate_records(self):
         user = User.first()
         article_count = user.articles.count()
-        # TODO: fix HasMany Relationship
+        # TODO: fix HasMany Relationship - save does not work
         # articles = [Article.hydrate({"name": "associate records"})]
-        # user.save_many("articles_rel", articles)
+        # user.save_many("articles", articles)
         # self.assertEqual(user.articles.count(), article_count+1)
 
     # def test_belongs_to_many(self):
