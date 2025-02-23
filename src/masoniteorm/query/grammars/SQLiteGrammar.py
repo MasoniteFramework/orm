36439219ca605b63ae5dd26b393821b5bd47070e
Kieren Eaton <499977+circulon@users.noreply.github.com>
2025-02-24 07:25:52 +0800

    Fixed OFFSET with sqlite throwing syntax error

--- src/masoniteorm/query/grammars/SQLiteGrammar.py
+++ src/masoniteorm/query/grammars/SQLiteGrammar.py
@@ -151,10 +151,12 @@ class SQLiteGrammar(BaseGrammar):
         return "{keyword} {foreign_table}{alias} {on}"
 
     def limit_string(self, offset=False):
+        if offset:
+            return ""
         return "LIMIT {limit}"
 
     def offset_string(self):
-        return "OFFSET {offset}"
+        return "LIMIT {limit} OFFSET {offset}"
 
     def first_where_string(self):
         return "WHERE"
@@ -215,3 +217,14 @@ class SQLiteGrammar(BaseGrammar):
 
     def compile_random(self):
         return "random()"
+
+    def process_offset(self):
+        """Compiles the offset expression.
+
+        Returns:
+            self
+        """
+        if not self._limit:
+            self._limit = int(-1)
+
+        return super().process_offset()
